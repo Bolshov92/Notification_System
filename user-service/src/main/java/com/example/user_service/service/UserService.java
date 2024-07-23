b7436c692509bcb0a6ba73d702323cac51f10e0b
Bolshov92 <bolshov92@gmail.com>
2024-07-23 21:43:50 +0100

    split evrithing to there packege

--- user-service/src/main/java/com/example/user_service/service/UserService.java
+++ user-service/src/main/java/com/example/user_service/service/UserService.java
@@ -4,8 +4,6 @@ import com.emergency.dto.UserAfterCreationDto;
 import com.emergency.dto.UserCreateDto;
 import com.emergency.entity.User;
 
-import java.util.List;
-
 public interface UserService {
     UserAfterCreationDto createUser(UserCreateDto user);
     User getUserById(String id);
