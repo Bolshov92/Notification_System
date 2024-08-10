afd3965cfbe7c9a258f01d671e09fe444a4d2281
Bolshov92 <bolshov92@gmail.com>
2024-08-10 17:31:53 +0100

    Integrate Kafka for inter-service communication in microservices

--- notification-service/src/main/java/com/example/notification_service/controller/NotificationController.java
+++ notification-service/src/main/java/com/example/notification_service/controller/NotificationController.java
@@ -2,27 +2,26 @@ package com.example.notification_service.controller;
 
 import com.example.notification_service.service.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/notifications")
 public class NotificationController {
+
     @Autowired
     private NotificationService notificationService;
 
     @PostMapping("/send-sms")
-    public Mono<Void> sendSms(@RequestParam String to, @RequestParam String message, ServerHttpRequest request) {
-        request.getQueryParams().forEach((paramName, paramValues) -> {
-            System.out.println("Parameter Name - " + paramName + ", Value - " + paramValues);
-        });
-
-        System.out.println("Received parameters - to: " + to + ", message: " + message);
-        notificationService.sendNotification(to, message);
-        return Mono.empty();
+    public ResponseEntity<String> sendSms(@RequestParam String to, @RequestParam String message) {
+        try {
+            notificationService.sendNotification(to, message);
+            return ResponseEntity.ok("SMS notification sent successfully.");
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Failed to send SMS notification: " + e.getMessage());
+        }
     }
 }
