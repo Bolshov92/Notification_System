0d84ee48807ee74088ec0bb07d9051d4ffa3ee0c
Bolshov92 <bolshov92@gmail.com>
2024-09-12 22:14:44 +0100

    added Listner for receiving requests and also a topic for responding to the notification service

--- event-service/src/main/java/com/example/event_service/repository/EventRepository.java
+++ event-service/src/main/java/com/example/event_service/repository/EventRepository.java
@@ -4,6 +4,9 @@ import com.example.event_service.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
+    List<Event> findByEventName(String eventName);
 }
--- event-service/src/main/java/com/example/event_service/service/impl/EventServiceImpl.java
+++ event-service/src/main/java/com/example/event_service/service/impl/EventServiceImpl.java
@@ -5,8 +5,12 @@ import com.example.event_service.entity.Event;
 import com.example.event_service.repository.EventRepository;
 import com.example.event_service.service.EventService;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -14,9 +18,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static com.mysql.cj.conf.PropertyKey.logger;
+
 @Service
 public class EventServiceImpl implements EventService {
-    private static final String TOPIC = "notification-topic";
+
+    private static final Logger logger = LoggerFactory.getLogger(EventServiceImpl.class);
+    private static final String EVENT_REQUEST_TOPIC = "event-request-topic";
+    private static final String EVENT_RESPONSE_TOPIC = "event-response-topic";
 
     @Autowired
     private EventRepository eventRepository;
@@ -47,5 +56,29 @@ public class EventServiceImpl implements EventService {
     @Override
     public void deleteEvent(Long id) {
         eventRepository.deleteById(id);
+    }
+    @KafkaListener(topics = EVENT_REQUEST_TOPIC, groupId = "event-group")
+    public void processEventRequest(String message) {
+        try {
+            JsonNode requestJson = objectMapper.readTree(message);
+            String eventName = requestJson.get("eventName").asText();
+
+            List<Event> events = eventRepository.findByEventName(eventName);
+            if (!events.isEmpty()) {
+                Event event = events.get(0);
+
+                Map<String, Object> response = new HashMap<>();
+                response.put("eventId", event.getId());
+                response.put("eventName", event.getEventName());
+                response.put("eventMessage", event.getEventMessage());
+
+                String responseJson = objectMapper.writeValueAsString(response);
+                kafkaTemplate.send(EVENT_RESPONSE_TOPIC, responseJson);
+            } else {
+                logger.warn("Событие не найдено: {}", eventName);
+            }
+        } catch (JsonProcessingException e) {
+            logger.error("Ошибка обработки запроса события: ", e);
+        }
     }
 }
