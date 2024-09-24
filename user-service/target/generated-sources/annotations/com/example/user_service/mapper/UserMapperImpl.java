7ae5c332cc8acc6b63a2187d8965f90e3d1a457a
Bolshov92 <bolshov92@gmail.com>
2024-09-24 22:20:12 +0100

    update DockerFile to mvn install

--- user-service/target/generated-sources/annotations/com/example/user_service/mapper/UserMapperImpl.java
+++ user-service/target/generated-sources/annotations/com/example/user_service/mapper/UserMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-09-23T23:59:45+0100",
+    date = "2024-09-24T22:15:24+0100",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.12 (Homebrew)"
 )
 @Component
