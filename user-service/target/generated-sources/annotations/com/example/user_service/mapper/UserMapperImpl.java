9583f59b1a3be75e0ff831edf26afaf110ad75e4
Bolshov92 <bolshov92@gmail.com>
2024-09-30 20:49:59 +0100

    DockerCompose

--- user-service/target/generated-sources/annotations/com/example/user_service/mapper/UserMapperImpl.java
+++ user-service/target/generated-sources/annotations/com/example/user_service/mapper/UserMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-09-30T20:22:21+0100",
+    date = "2024-09-30T20:47:41+0100",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.12 (Homebrew)"
 )
 @Component
