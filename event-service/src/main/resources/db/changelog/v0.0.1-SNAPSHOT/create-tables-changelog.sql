d4e88774c23d546b80a94619214352c0d7aba77f
Bolshov92 <bolshov92@gmail.com>
2024-10-20 19:21:18 +0100

    update EVENT service and File service

--- event-service/src/main/resources/db/changelog/v0.0.1-SNAPSHOT/create-tables-changelog.sql
+++ event-service/src/main/resources/db/changelog/v0.0.1-SNAPSHOT/create-tables-changelog.sql
@@ -1,6 +1,6 @@
 CREATE TABLE event
 (
     id            BIGINT AUTO_INCREMENT PRIMARY KEY,
-    event_name    VARCHAR(255) NOT NULL,
+    event_name    VARCHAR(255) NOT NULL UNIQUE,
     event_message TEXT         NOT NULL
 );
--- file-service/src/main/resources/db/changelog/v0.0.1-SNAPSHOT/create-tables-changelog.sql
+++ file-service/src/main/resources/db/changelog/v0.0.1-SNAPSHOT/create-tables-changelog.sql
@@ -1,7 +1,7 @@
 CREATE TABLE files
 (
     id          BIGINT AUTO_INCREMENT PRIMARY KEY,
-    file_name   VARCHAR(255),
+    file_name   VARCHAR(255) UNIQUE,
     file_type   VARCHAR(255),
     file_path   VARCHAR(255),
     upload_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
