c3138db7793ddc268292cb03d8917ef2879f8def
Bolshov92 <bolshov92@gmail.com>
2024-10-29 22:53:50 +0000

    insert in table users ADMIN

--- user-service/src/main/resources/db/changelog/v0.0.1-SNAPSHOT/insert-data-userInfo.sql
+++ user-service/src/main/resources/db/changelog/v0.0.1-SNAPSHOT/insert-data-userInfo.sql
@@ -1,2 +1,2 @@
 INSERT INTO user_info (id, user_name, password, role_id, user_id)
-VALUES (2, 'Slava', '$2a$12$i6NkMo61D7uzZfaTAvRasulqLjavoqDsVgl3gDeEcV2rqSntxI4P2', 3, 1);
+VALUES (2, 'ADMIN', '$2a$12$eGlppNTSXDuONWZWpmvmP.S.rBmwIFVJlkV2D4hoy0OXsEfywavGm', 3, 1);
